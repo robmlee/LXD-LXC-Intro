1c6afddade7c2258acabec08e62bf7e05f788b7d
Robert 蓉爸 <1572639+robmlee@users.noreply.github.com>
2025-03-29 17:38:21 +0800

    Update os-rpi-upd.sh

--- os-rpi-upd.sh
+++ os-rpi-upd.sh
@@ -4,8 +4,8 @@ logFile=`basename $0 | cut -d. -f1`.log
 echo | tee -a $logFile
 uname -a | tee -a $logFile
 echo | tee -a $logFile
-sudo apt update | tee -a $logFile;sudo apt full-upgrade -y | tee -a $logFile
-sudo apt autoremove -y | tee -a $logFile; sudo apt autoclean | tee -a $logFile
+sudo apt-get update | tee -a $logFile;sudo apt-get full-upgrade -y | tee -a $logFile
+sudo apt-get autoremove -y | tee -a $logFile; sudo apt-get autoclean | tee -a $logFile
 
 # check bootloader version 
 sudo rpi-eeprom-update | tee -a $logFile
